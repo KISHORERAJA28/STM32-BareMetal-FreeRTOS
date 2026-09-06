4f6d45252e46314f69271e1f501b756f34a61d44
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-09-06 16:00:24 +0530

    Delete hardware_registers.h

--- hardware_registers.h
+++
@@ -1,9 +0,0 @@
-// hardware_registers.h
-// Custom register abstractions for bare-metal memory execution
-#ifndef HARDWARE_REGISTERS_H
-#define HARDWARE_REGISTERS_H
-
-#define SENSOR_DATA_REG  (*(volatile uint32_t*)(0x40020C24))
-#define SYSTEM_STATUS_REG (*(volatile uint32_t*)(0x40020C28))
-
-#endif
