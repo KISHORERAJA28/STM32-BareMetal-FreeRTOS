c788df779ee93f31ff0161e9a992356212cf3dbf
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-09-22 22:19:00 +0530

    Update main.c

--- Core/main.c
+++ Core/main.c
@@ -3,7 +3,7 @@
  @brief          : Bare-Metal Architecture, Interrupt-Driven SPI/DMA, and
                   Deterministic FreeRTOS Task Scheduling
  @target         : STM32 (ARM Cortex-M4 Architecture)
- * ******************************************************************************
+ 
 
 #include <stdint.h>
 #include "FreeRTOS.h"
@@ -22,24 +22,24 @@ static uint8_t telemetry_buffer[8] = {1, 2, 3, 4, 5, 6, 7, 8};
 
 int main(void)
 {
-    //1: Bare-Metal Hardware Register Configuration.
-    RCC_AHB1ENR |= (1 << 3);   //Enable clock gating for GPIOD,
-    GPIOD_MODER |= (1 << 24); //GPIOD Pin 12 as general-purpose output,
+    //Bare-Metal Hardware Register Configuration.
+    RCC_AHB1ENR |= (1 << 3);   //Enable clock gating for GPIOD.
+    GPIOD_MODER |= (1 << 24); //GPIOD Pin 12 as general-purpose output.
 
-    //2: Interrupt-driven, DMA-backed SPI1 initialization,
+    //Interrupt-driven, DMA-backed SPI1 initialization.
     spi1_dma_init();
 
-    //3: RTOS synchronization primitive for ISR -> task handoff,
+    //RTOS synchronization primitive for ISR -> task handoff.
     xSpiDmaCompleteSemaphore = xSemaphoreCreateBinary();
 
-    //4: Initialize deterministic RTOS multi-threaded tasks,
+    //Initialize deterministic RTOS multi-threaded tasks.
     xTaskCreate(vSensorTelemetryTask, "Sensor_Read", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
     xTaskCreate(vActuatorControlTask, "Actuator_Ctrl", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
     xTaskCreate(vSpiDmaConsumerTask, "SPI_DMA_Consumer", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
 
-    //5: Core kernel boot sequence,
+    //Core kernel boot sequence.
     vTaskStartScheduler();
-    while (1); //Only reached if scheduler init fails,
+    while (1); //Only reached if scheduler init fails..
 }
 
 void vSensorTelemetryTask(void *pvParameters)
