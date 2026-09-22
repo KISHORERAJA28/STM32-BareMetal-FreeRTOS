5258dd42325857fd983c4222ac1e96d17dd225f0
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-09-22 22:33:38 +0530

    Update main.c

--- Core/main.c
+++ Core/main.c
@@ -15,24 +15,18 @@ static uint8_t telemetry_buffer[8] = {1, 2, 3, 4, 5, 6, 7, 8};
 
 int main(void)
 {
-    //Bare-Metal Hardware Configuration.
     RCC_AHB1ENR |= (1 << 3);   
     GPIOD_MODER |= (1 << 24); 
 
-    //Interrupt-driven,DMA-backedSPI1 initialization.
     spi1_dma_init();
-
-    //RTOS synchronization primitive for ISR -> task handoff.
     xSpiDmaCompleteSemaphore = xSemaphoreCreateBinary();
 
-    //Initialize deterministic RTOS multi-threaded tasks.
     xTaskCreate(vSensorTelemetryTask, "Sensor_Read", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
     xTaskCreate(vActuatorControlTask, "Actuator_Ctrl", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
     xTaskCreate(vSpiDmaConsumerTask, "SPI_DMA_Consumer", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
 
-    //Core kernel boot sequence.
     vTaskStartScheduler();
-    while (1); //if init fails..
+    while (1); 
 }
 
 void vSensorTelemetryTask(void *pvParameters)
@@ -50,8 +44,6 @@ void vActuatorControlTask(void *pvParameters)
         vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
-
-//demonstrates interrupt -> RTOS task synchronization, not polling.
 void vSpiDmaConsumerTask(void *pvParameters)
 {
     while (1) {
